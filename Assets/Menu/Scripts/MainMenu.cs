f24b289f63bf8940764bedc2a1abb073185c990b
SeaofChaos <46305438+SeaofChaos@users.noreply.github.com>
2021-12-06 01:27:22 -0500

    Updated volume control

--- Assets/Menu/Scripts/MainMenu.cs
+++ Assets/Menu/Scripts/MainMenu.cs
@@ -5,9 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField]
+    public CanvasGroup canvas;
+
     public void PlayGame(){
         //Load next scene in the scene order. Can manually change which scene is loaded depending on parameter value.
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); 
+    }
+
+    void Awake(){
+        hideOptions();
+    }
+
+    public void hideOptions(){
+        canvas.alpha = 0f;
+        canvas.blocksRaycasts = false;
+    }
+
+    public void showOptions(){
+        canvas.alpha = 1f;
+        canvas.blocksRaycasts = true;
     }
 
     public void QuitGame(){
